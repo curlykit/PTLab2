ecdd635ca0c13c2bdede6dedae42c5b63fb99d46
Keith <katherine.zatsepilova@yandex.ru>
2025-10-26 00:49:25 +0300

    Настройка Django проекта: база данных, админка, товары

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Product  # Импортируем модель Product
 
-# Register your models here.
+# Регистрируем модель Product в админке
+admin.site.register(Product)
--- tplab2/settings.py
+++ tplab2/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'tplab2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_db',
-        'USER' : 'postgres',
-        'PASSWORD' : os.environ['DATABASE_PASSWORD'] if 'DATABASE_PASSWORD' in os.environ else '',
-        'HOST' : 'localhost',
-        'PORT' : '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'ps_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
