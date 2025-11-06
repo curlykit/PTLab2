dcfd14180f1f591cfaaa2a9a13fe7927641ed2d4
Keith <katherine.zatsepilova@yandex.ru>
2025-11-06 13:10:10 +0300

    Register Purchase model in admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Product  # Импортируем модель Product
+from .models import Product, Purchase  # Импортируем модель Product
 
 # Регистрируем модель Product в админке
 admin.site.register(Product)
+admin.site.register(Purchase)
